9ddd155293ad485ed9cb0369c997d7dc2854fbe1
J1shen <133644840+J1shen@users.noreply.github.com>
2023-07-30 12:53:29 +0800

    Update Makeup.py

--- Makeup.py
+++ Makeup.py
@@ -1,6 +1,118 @@
-import cv2,imutils,dlib
+import cv2,imutils,dlib,math
 import numpy as np
 #from utils import face_thin_auto, SharpenImage
+def SharpenImage(src):
+    blur_img = cv2.GaussianBlur(src, (0, 0), 5)
+    usm = cv2.addWeighted(src, 1.5, blur_img, -0.5, 0)
+    return usm
+
+
+def BilinearInsert(src, ux, uy):
+    # 双线性插值法
+    w, h, c = src.shape
+    if c == 3:
+        x1 = int(ux)
+        x2 = x1+1
+        y1 = int(uy)
+        y2 = y1+1
+
+        part1 = src[y1, x1].astype(np.float32)*(float(x2)-ux)*(float(y2)-uy)
+        part2 = src[y1, x2].astype(np.float32)*(ux-float(x1))*(float(y2)-uy)
+        part3 = src[y2, x1].astype(np.float32) * (float(x2) - ux)*(uy-float(y1))
+        part4 = src[y2, x2].astype(np.float32) * \
+            (ux-float(x1)) * (uy - float(y1))
+
+        insertValue = part1+part2+part3+part4
+
+        return insertValue.astype(np.int8)
+
+
+def localTranslationWarp(srcImg, startX, startY, endX, endY, radius):
+
+    ddradius = float(radius * radius)
+    copyImg = np.zeros(srcImg.shape, np.uint8)
+    copyImg = srcImg.copy()
+
+    # 计算公式中的|m-c|^2
+    ddmc = (endX - startX) * (endX - startX) + \
+        (endY - startY) * (endY - startY)
+    H, W, C = srcImg.shape
+    for i in range(W):
+        for j in range(H):
+            # 计算该点是否在形变圆的范围之内
+            # 优化，第一步，直接判断是会在（startX,startY)的矩阵框中
+            if math.fabs(i-startX) > radius and math.fabs(j-startY) > radius:
+                continue
+
+            distance = (i - startX) * (i - startX) + \
+                (j - startY) * (j - startY)
+
+            if(distance < ddradius):
+                # 计算出（i,j）坐标的原坐标
+                # 计算公式中右边平方号里的部分
+                ratio = (ddradius-distance) / (ddradius - distance + ddmc)
+                ratio = ratio * ratio
+
+                # 映射原位置
+                UX = i - ratio * (endX - startX)
+                UY = j - ratio * (endY - startY)
+
+                # 根据双线性插值法得到UX，UY的值
+                value = BilinearInsert(srcImg, UX, UY)
+                # 改变当前 i ，j的值
+                copyImg[j, i] = value
+
+    return copyImg
+
+
+def landmark_dec_dlib_fun(img_src, detector, predictor):
+    img_gray = cv2.cvtColor(img_src, cv2.COLOR_BGR2GRAY)
+
+    land_marks = []
+
+    rects = detector(img_gray, 0)
+
+    for i in range(len(rects)):
+        land_marks_node = np.matrix(
+            [[p.x, p.y] for p in predictor(img_gray, rects[i]).parts()])
+        land_marks.append(land_marks_node)
+
+    return land_marks
+
+
+def face_thin_auto(src, detector, predictor):
+
+    landmarks = landmark_dec_dlib_fun(src, detector, predictor)
+
+    # 如果未检测到人脸关键点，就不进行瘦脸
+    if len(landmarks) == 0:
+        return src
+
+    for landmarks_node in landmarks:
+        left_landmark = landmarks_node[3]
+        left_landmark_down = landmarks_node[5]
+
+        right_landmark = landmarks_node[13]
+        right_landmark_down = landmarks_node[15]
+
+        endPt = landmarks_node[30]
+
+        # 计算第4个点到第6个点的距离作为瘦脸距离
+        r_left = math.sqrt((left_landmark[0, 0]-left_landmark_down[0, 0])*(left_landmark[0, 0]-left_landmark_down[0, 0]) +
+                           (left_landmark[0, 1] - left_landmark_down[0, 1]) * (left_landmark[0, 1] - left_landmark_down[0, 1]))
+
+        # 计算第14个点到第16个点的距离作为瘦脸距离
+        r_right = math.sqrt((right_landmark[0, 0]-right_landmark_down[0, 0])*(right_landmark[0, 0]-right_landmark_down[0, 0]) +
+                            (right_landmark[0, 1] - right_landmark_down[0, 1]) * (right_landmark[0, 1] - right_landmark_down[0, 1]))
+
+        # 瘦左边脸
+        thin_image = localTranslationWarp(
+            src, left_landmark[0, 0], left_landmark[0, 1], endPt[0, 0], endPt[0, 1], r_left)
+        # 瘦右边脸
+        thin_image = localTranslationWarp(
+            thin_image, right_landmark[0, 0], right_landmark[0, 1], endPt[0, 0], endPt[0, 1], r_right)
+
+        return thin_image
 
 class Organ():
     def __init__(self, img, img_h,landmark, name, ksize=None):
@@ -129,7 +241,7 @@ class Organ():
         sharp = cv2.filter2D(self.patch_bgr, -1, kernel)
         self.patch_bgr[index] = np.minimum(
             ((1-rate)*self.patch_bgr)[index]+sharp[index]*rate, 255).astype('uint8')
-            
+
 class Forehead(Organ):
     def __init__(self, im_bgr, im_hsv,  landmark, mask_organs, name, ksize=None):
         self.mask_organs = mask_organs
@@ -252,11 +364,11 @@ class Making_up():
         if len(rects) < 1:
             print('no face detected')
             return
-        
+
         self.faces = [Face(self.im_bgr ,self.im_hsv ,np.array([[p.x, p.y] for p in self.predictor(self.im_bgr , rect).parts()]), i) for i, rect in enumerate(rects)]
 
         return self.im_bgr
-    
+
     def _mapfaces(self, fun, value):
         '''
         对每张脸进行迭代操作
@@ -264,31 +376,37 @@ class Making_up():
         for face in self.faces:
             fun(face, value)
 
-    def _Laplace(self,val):
+    def _Laplace(self,val,batch=False):
         value = min(1, max(val, 0))
         kernel = np.array([[0, -1, 0], [0, 5, 0], [0, -1, 0]])
-        print('-[INFO] laplace:', value)
-        self.img_bgr = SharpenImage(self.img_bgr)
+        if batch == False:
+          print('-[INFO] laplace:', value)
+        self.im_bgr = SharpenImage(self.im_bgr)
         # self.temp_bgr = cv2.filter2D(self.temp_bgr, -1, kernel)
-        self.img_bgr = np.minimum(self.img_bgr, 255).astype('uint8')
+        self.im_bgr = np.minimum(self.im_bgr, 255).astype('uint8')
 
-    def _Thin(self, val):
+    def _Thin(self, val, batch=False):
         value = min(1, max(val, 0))
-        print('-[INFO] thin:', value)
-        self.img_bgr = face_thin_auto(self.img_bgr, self.detector, self.predictor)
+        if batch == False:
+          print('-[INFO] thin:', value)
+        self.im_bgr = face_thin_auto(self.im_bgr, self.detector, self.predictor)
+        rects = self.detector(self.im_bgr , 1)
+        self.faces = [Face(self.im_bgr ,self.im_hsv ,np.array([[p.x, p.y] for p in self.predictor(self.im_bgr , rect).parts()]), i) for i, rect in enumerate(rects)]
 
-    def _sharpen(self,val):
+    def _sharpen(self,val, batch=False):
         value = min(1, max(val, 0))
-        print('-[INFO] sharpen:', value)
+        if batch == False:
+          print('-[INFO] sharpen:', value)
 
         def fun(face, value):
-            face.organs['left eye'].sharpen(value, confirm=False)
-            face.organs['right eye'].sharpen(value, confirm=False)
+            face.organs['left eye'].sharpen(value)
+            face.organs['right eye'].sharpen(value)
         self._mapfaces(fun, value)
 
-    def _whitening(self, val):
+    def _whitening(self, val, batch=False):
         value = min(1, max(val, 0))
-        print('-[INFO] whitening:', value)
+        if batch == False:
+          print('-[INFO] whitening:', value)
 
         def fun(face,value):
             face.organs['left eye'].whitening(value)
@@ -301,17 +419,19 @@ class Making_up():
             face.whitening(value)
         self._mapfaces(fun, value)
 
-    def _brightening(self, val):
+    def _brightening(self, val, batch=False):
         value = min(1, max(val, 0))
-        print('-[INFO] brightening:', value)
+        if batch == False:
+          print('-[INFO] brightening:', value)
 
         def fun(face, value):
             face.organs['mouth'].brightening(value)
         self._mapfaces(fun, value)
 
-    def _smooth(self,val):
+    def _smooth(self,val,batch=False):
         value = min(1, max(val, 0))
-        print('-[INFO] smooth:', value)
+        if batch == False:
+          print('-[INFO] smooth:', value)
 
         def fun(face, value):
             face.smooth(value)
@@ -319,3 +439,16 @@ class Making_up():
             face.organs['forehead'].smooth(value*3/2)
             face.organs['mouth'].smooth(value)
         self._mapfaces(fun, value)
+
+    def beautify(self,img_pth):
+        im_bgr = self.read_img(img_pth)
+        img_ori = cv2.cvtColor(im_bgr,cv2.COLOR_BGR2RGB)
+        self._Thin(1)
+        self._whitening(0.2)
+        self._brightening(0.2)
+        self._smooth(0.2)
+        self._sharpen(0.2)
+        self._Laplace(0.1)
+        img_new = cv2.cvtColor(self.im_bgr,cv2.COLOR_BGR2RGB)
+        img = np.concatenate((img_ori,img_new),axis=1)
+        return img
