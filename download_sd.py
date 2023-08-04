6fae12589c9822cc1a07282ccb4e07eb92967c40
tonyxu <tonyxuhit@gmail.com>
2023-08-04 16:25:28 +0800

    update

--- download_sd.py
+++ download_sd.py
@@ -1,9 +1,12 @@
-from huggingface_hub import hf_hub_download
+from huggingface_hub import hf_hub_download,snapshot_download
+snapshot_download(repo_id="stabilityai/stable-diffusion-2-1-base",
+                  cache_dir='./checkpoints')
 """
 hf_hub_download(repo_id="stabilityai/stable-diffusion-2-1-base", 
                 filename="v2-1_512-ema-pruned.ckpt",
                 cache_dir='./checkpoints')
-"""
+
 hf_hub_download(repo_id="runwayml/stable-diffusion-v1-5", 
                 filename="v1-5-pruned.ckpt",
                 cache_dir='./checkpoints')
+"""
--- download_stablediff.py
+++
@@ -1,3 +0,0 @@
-from huggingface_hub import snapshot_download
-snapshot_download(repo_id="stabilityai/stable-diffusion-2-1-base",
-                  cache_dir='./checkpoints')
